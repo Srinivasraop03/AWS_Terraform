e3ad2405bfb1482c4a7749c073c9d0de17103ad0
Srinivasraop03 <srinivas@fyndme.net>
2025-11-09 13:05:17 -0600

    updated files

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0ecb62995f68bb549" # us-west-1
+  ami           = "ami-04b70fa74e45c3917" # Ubuntu 22.04 LTS in us-east-1
   instance_type = "t3.micro"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
